a9df4d1f26a2885e2739c49fe07fd1c967ae2a2b
DanielEduardoBarba <danieladuardobarba@gmail.com>
2023-12-03 17:33:25 -0500

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/bash
 
-avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o led.o led.c 
-avr-gcc -o led.bin led.o
-avr-objcopy -O ihex -R .eeprom led.bin led.hex
-sudo avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:led.hex
+# Specify the correct serial port for Arduino Mega (change this to your port)
+SERIAL_PORT=/dev/ttyACM0
+
+# Define the microcontroller type for Arduino Mega (ATmega2560)
+MCU=atmega328p
+
+# Define the clock frequency for Arduino Mega (usually 16MHz)
+F_CPU=16000000UL
+
+# Source file (change this to your actual source file name)
+SOURCE_FILE=led.c
+
+# Output file names
+OUTPUT_FILE=led
+
+# Compile the source code
+avr-gcc -Os -DF_CPU=$F_CPU -mmcu=$MCU -c -o $OUTPUT_FILE.o $SOURCE_FILE
+
+
+# Link the object file to create the binary
+avr-gcc -o $OUTPUT_FILE.bin $OUTPUT_FILE.o
+
+# Create a hex file from the binary
+avr-objcopy -O ihex -R .eeprom $OUTPUT_FILE.bin $OUTPUT_FILE.hex
+
+
+# Use avrdude to upload the hex file to the Arduino Mega
+sudo avrdude -F -V -c arduino -p $MCU -P $SERIAL_PORT -b 115200 -U flash:w:$OUTPUT_FILE.hex
